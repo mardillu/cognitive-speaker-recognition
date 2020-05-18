fe7018ad6e6b885ba3187445b4a9bbfba13572d8
Ezekiel Sebastine <ezekiel@farmerline.co>
2020-05-18 03:34:00 +0000

    initial commit. fixed manifest error

---
+++ lib/build/generated/source/buildConfig/debug/com/microsoft/cognitive/speakerrecognition/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.microsoft.cognitive.speakerrecognition;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.microsoft.cognitive.speakerrecognition";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.microsoft.cognitive.speakerrecognition";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
